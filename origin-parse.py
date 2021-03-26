22468fb4d256b63b8f2a772fe29b66ea259115e2
Tanner C. Myers <tannercmyers@gmail.com>
2021-03-26 18:59:41 -0500

    Adding docstrings

--- origin-parse.py
+++ origin-parse.py
@@ -9,19 +9,42 @@ def find_mention(in_stream,
     """
 
     """
-#print('Opening origin.txt')
-#with open('origin.txt', 'r') as in_stream:
-#    print('Opening origin-output.txt')
-#    with open('origin-output.txt', 'w') as out_stream:
-#        for line in in_stream:
-        #    line = line.strip()
-#            word_list = line.split()
-#            word_list.sort()
-#            for word in word_list:
-#                out_stream.write('{0}\n'.format(word))
-#print("Done!")
-#print('origin.txt is closed?', in_stream.closed)
-#print('origin-output.txt is closed?', out_stream.closed)
+
+
+def record_occurrences(in_stream, out_stream,
+        target_regex,
+        start_regex = None,
+        stop_regex = None):
+    """
+    Counts the occurrences of words matching the targeted regular expression
+    and records the line number it occurs on.
+    ------------------------------------------------------------------------
+    Parameters
+    ------------------------------------------------------------------------
+    in_stream : A file object
+        Input, in this case, 'On the Origin of Species'
+    out_stream : A file object
+        Output containing line number and words matching regular expression
+    target_regex : A regular expression object
+        Search item in `in_stream`
+    start_regex : A regular expression object
+        Where the targeted regular expression object can begin to be searched for
+        in `in_stream`
+    stop_regex :
+        Where the targeted regular expression object can no longer be searched for
+        in `in_stream`
+    """    
+    occurrences = 0
+    for line_index, match_obj in find_mention(in_stream, target_regex,
+            start_regex, stop_regex):
+        occurrences += 1
+        for target_str in match_obj.groups():
+            out_stream.write("{line_num}\t{string}\n".format(
+                    line_num = line_index + 1,
+                    string = target_str))
+    return occurrences
+
+
 
 if __name__ == '__main__':
     target_pattern = re.compile(r'(^\w*herit\w*$)', re.IGNORECASE)
