860dcf38c9aeebdb9ccee93dccca5432002443db
Tanner C. Myers <tannercmyers@gmail.com>
2021-03-26 18:00:02 -0500

    Removing line.strip() line

--- origin-parse.py
+++ origin-parse.py
@@ -5,7 +5,7 @@ with open('origin.txt', 'r') as in_stream:
     print('Opening origin-output.txt')
     with open('origin-output.txt', 'w') as out_stream:
         for line in in_stream:
-            line = line.strip()
+        #    line = line.strip()
             word_list = line.split()
             word_list.sort()
             for word in word_list:
